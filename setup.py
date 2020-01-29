2033d16e4a0fa650fbbe4160f6ba80cd0e2a2306
pylover <vahid.mardani@gmail.com>
2020-01-29 23:46:16 +0330

    Python packaging

--- setup.py
+++ setup.py
@@ -20,9 +20,20 @@ setup(
     version=version,
     description='vURL.ir shortener CLI',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=dependencies,
     py_modules=['vurl'],
     entry_points=dict(console_scripts='vurl=vurlcli:VURL.quickstart'),
     license='MIT',
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development',
+    ]
 )
 
